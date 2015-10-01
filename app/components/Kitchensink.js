007c4b9ef79b3b2ecbe210ee5fe4e40494b86435
Mikkel Malmberg <mikkel@brnbw.com>
2015-10-01 12:59:08 +0200

    Extract Button to its own component

--- app/components/Kitchensink.js
+++ app/components/Kitchensink.js
@@ -1,8 +1,9 @@
-var React = require('react')
+import React from 'react'
+import Button from './Button'
+
 require('../scss/PageTitle.scss')
 require('../scss/SectionTitle.scss')
 require('../scss/Copy.scss')
-require('../scss/Button.scss')
 
 var Kitchensink = React.createClass({
   render: function() {
@@ -11,14 +12,14 @@ var Kitchensink = React.createClass({
         <div className="PageTitle PageTitle--boxed"><h1>Kitchensink</h1></div>
         <div className="SectionTitle"><h2>Buttons</h2></div>
         <div className="Copy">
-          <p><a className="Button Button--small" href="#">Small button</a></p>
-          <p><a className="Button" href="#">Default button</a></p>
-          <p><a className="Button Button--disabled" href="#">Disabled button</a></p>
-          <p><a className="Button Button--red" href="#">Red button</a></p>
-          <p><a className="Button Button--light" href="#">Light button</a></p>
-          <p><a className="Button Button--shadow" href="#">Default with shadow</a></p>
-          <p><a className="Button Button--red Button--darkShadow" href="#">Red with dark shadow</a></p>
-          <p><a className="Button Button--shadow Button--fullWidth" href="#">Full width with shadow</a></p>
+          <p><Button type='small'>Small button</Button></p>
+          <p><Button>Default Button</Button></p>
+          <p><Button disabled>Disabled button</Button></p>
+          <p><Button type='red'>Red button</Button></p>
+          <p><Button type='light'>Light button</Button></p>
+          <p><Button type='shadow'>Default with shadow</Button></p>
+          <p><Button type='red darkShadow'>Red with dark shadow</Button></p>
+          <p><Button type='shadow fullWidth'>Full width with shadow</Button></p>
         </div>
 
         <div className="SectionTitle"><h2>Headings</h2></div>
