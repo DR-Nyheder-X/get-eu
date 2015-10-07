089ecc87cd61a9703c5da2c11a32411f469163f6
Mikkel Malmberg <mikkel@brnbw.com>
2015-10-07 15:15:48 +0200

    Add SET_POINTS action

--- client/reducers/progress.js
+++ client/reducers/progress.js
@@ -1,9 +1,17 @@
+import { SET_POINTS, setPoints } from '../actions'
+
 const initialState = {
   points: 0
 }
 
 export default function progressReducer (state = initialState, action) {
   switch (action.type) {
+    case SET_POINTS: {
+      return {
+        ...state,
+        points: action.points
+      }
+    }
     default: {
       return state
     }
