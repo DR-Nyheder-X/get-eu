e246e3398a7cdb1a72f7f52e984470400c28d950
Mikkel Malmberg <mikkel@brnbw.com>
2015-10-29 14:32:59 +0100

    Simplify Term component

--- client/components/Term.js
+++ client/components/Term.js
@@ -1,18 +1,11 @@
-import React, { Component, PropTypes } from 'react'
+import React, { Component } from 'react'
 
 import '../scss/Term.scss'
 
 export default class Term extends Component {
-  static get propTypes () {
-    return {
-      type: PropTypes.string,
-      className: PropTypes.string
-    }
-  }
   render () {
-    const cls = 'Term'
     return (
-      <li className={cls} {...this.props}></li>
+      <li className='Term' {...this.props}></li>
     )
   }
 }
