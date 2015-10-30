a8fa763d21712c4d743a56baec239383328e1689
Jacques Mourier <jacquesmourier@gmail.com>
2015-10-30 13:21:23 +0100

    done page done

--- client/components/Done.js
+++ client/components/Done.js
@@ -4,6 +4,8 @@ import { Tiles, Tile } from './Tiles'
 import Repo from '../Repo'
 import { connect } from 'react-redux'
 
+import '../scss/Done.scss'
+
 @connect(state => ({
   type: state.router.params.type,
   progress: state.progress
@@ -17,12 +19,22 @@ export default class Done extends Component {
 
     return (
       <div className='Done'>
-        <PageTitle type='small centered'>
+        <PageTitle type='small centered shadow'>
           Det var det!
         </PageTitle>
         <p className='Done-message'>
           Du klarede dig gennem alle spørgsmål. Nu kan du sætte dit kryds d. 3. december med god, velinformeret samvittighed. Ole Ryborg er stolt af dig.
         </p>
+        <div className='Done-cta'>
+          <a href="#">
+            <i></i>
+            <em>Vil du ha mere?</em>
+            <b>Dyk ned i leksikonet</b>
+          </a>
+        </div>
+        <div className='Done-reset'>
+          <a href="#"><i></i>Start forfra</a>
+        </div>
         <Tiles>
           {categories.map(c => (
             <Tile key={c.id} category={c} progress={progress} />
