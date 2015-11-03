eba2d1c17a3fa3b4488bcb01c38a96dfd8cb56a8
Mikkel Malmberg <mikkel@brnbw.com>
2015-11-03 11:33:58 +0100

    Fix routes

--- client/routes.js
+++ client/routes.js
@@ -17,16 +17,16 @@ const routes = (
   <Route path='/'>
     <IndexRoute component={Start} />
     <Route path='kitchensink' component={Kitchensink} />
-    <Route path='quiz' component={App}>
-      <IndexRoute component={Quiz} />
+    <Route component={App}>
+      <Route path='/quiz' component={Quiz} />
       <Route path='/quiz/:type' component={Category} />
       <Route path='/quiz/:type/done' component={CategoryDone} />
       <Route path='/quiz/:type/:step/question' component={Question} />
       <Route path='/quiz/:type/:step' component={Step} />
+      <Route path='points' component={Points} />
+      <Route path='the_end' component={Done} />
+      <Route path='*' component={NotFound} />
     </Route>
-    <Route path='points' component={Points} />
-    <Route path='the_end' component={Done} />
-    <Route path='*' component={NotFound} />
   </Route>
 )
 
