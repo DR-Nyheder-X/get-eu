a49db5a7fe535d9d4b0bda38558eb11c6d3dbc58
Mikkel Malmberg <mikkel@brnbw.com>
2015-11-13 15:08:49 +0100

    Redirect to root if landing on sub-path with no answered questions

--- client/quiz.js
+++ client/quiz.js
@@ -6,6 +6,7 @@ import { render } from 'react-dom'
 import { Provider } from 'react-redux'
 import { ReduxRouter } from 'redux-router'
 import { RESET_PROGRESS } from './actions'
+import { pushState } from 'redux-router'
 
 // STORE ------------------------------------------------
 
@@ -48,4 +49,12 @@ if (module.hot) {
 window.resetProgress = function () {
   store.dispatch({ type: RESET_PROGRESS })
   return true
+}
+
+// Redirect to root if no points in progress
+const state = store.getState()
+if (state.progress.completedStepIds.length === 0) {
+  if (!state.router.location.pathname.match(/^\/$/)) {
+    store.dispatch(pushState(null, '/'))
+  }
 }
